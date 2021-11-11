2f744bca649136edba8b67657329704dc5545177
11carmel11 <11carmrl11@gmail.com>
2021-11-11 11:23:21 +0200

    making the student schema

--- 18.Mongo-Weekend/model/student.js
+++ 18.Mongo-Weekend/model/student.js
@@ -8,3 +8,28 @@ mongoose
   })
   .then(() => console.log("Database connected!"))
   .catch((err) => console.log(err));
+
+const StudentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  surName: {
+    type: String,
+    required: true,
+  },
+  birth: {
+    type: Date,
+    required: true,
+  },
+  phone: {
+    type: String,
+    unique: true,
+  },
+  gender: {
+    type: String,
+  },
+  courses: [{ type: String }],
+});
+
+const Student = mongoose.model("Student", StudentSchema);
