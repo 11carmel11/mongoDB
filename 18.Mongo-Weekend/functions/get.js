09824e6fb9ddf985bc725347ac73772b1810616e
11carmel11 <11carmrl11@gmail.com>
2021-11-11 17:43:17 +0200

    added delete functions

--- 18.Mongo-Weekend/functions/get.js
+++ 18.Mongo-Weekend/functions/get.js
@@ -6,38 +6,28 @@ const getAllStudents = async () => {
 };
 
 // get all students with name set to "Ido"
-const getAllIdo = () => {
-  Student.find({ name: "Ido" }).then((students) => {
-    return students;
-  });
+const getAllIdo = async () => {
+  return await Student.find({ name: "Ido" });
 };
 
 // get all students where courses include "Law"
-const getAllLawStudents = () => {
-  Student.find({ courses: "Law" }).then((students) => {
-    return students;
-  });
+const getAllLawStudents = async () => {
+  return await Student.find({ courses: "Law" });
 };
 
 // get all students where courses include "Java" and gender set to "Female"
-const getAllFemaleJavaStudents = () => {
-  Student.find({ $and: [{ courses: "Java" }, { gender: "Female" }] }).then(
-    (students) => {
-      return students;
-    }
-  );
+const getAllFemaleJavaStudents = async () => {
+  return await Student.find({
+    $and: [{ courses: "Java" }, { gender: "Female" }],
+  });
 };
 
 // get all students where birth > 05/05/1998
-const getAllChildren = () => {
-  Student.find({ birth: { $lt: new Date("1998/05/05Z") } }).then((students) => {
-    return students;
-  });
+const getAllChildren = async () => {
+  return await Student.find({ birth: { $gt: new Date("1998/05/05Z") } });
 };
 
 // get all students where phone starts with 054
-const getAllStudentsWith054 = () => {
-  Student.find({ phone: /^054/ }).then((students) => {
-    return students;
-  });
+const getAllStudentsWith054 = async () => {
+  return await Student.find({ phone: /^054/ });
 };
--- 18.Mongo-Weekend/functions/update.js
+++ 18.Mongo-Weekend/functions/update.js
@@ -1,41 +1,28 @@
 const Student = require("../model/student");
 
 // add a JavaScript course to the students where name set to "Yahalom"
-//#region optional:
-// const addJSCourseToYahalom = () => {
-//     Student.updateMany({ name: "Yahalom" }, { course: course.push("JavaScript") })
-//     .then(() => {
-//       return "updated";
-//     })
-//     .catch(() => {
-//       return "failed";
-//     });
-// };
-//#endregion
-const addJSCourseToYahalom = () => {
-  Student.find({ name: "Yahalom" })
-    .then((students) => {
-      students.forEach((student) => {
-        Student.findOneAndUpdate(
-          { phone: student.phone },
-          { courses: student.courses.push("JavaScript") }
-        ).then(() => {
-          return "updated";
-        });
-      });
-    })
-    .catch(() => {
-      return "failed";
+const addJSCourseToYahalom = async () => {
+  try {
+    const students = await Student.find({ name: "Yahalom" });
+    students.forEach(async (student) => {
+      const { courses } = student;
+      courses.push("JavaScript");
+      await Student.findOneAndUpdate({ phone: student.phone }, { courses });
     });
+    return "updated";
+  } catch (error) {
+    return false;
+  }
 };
-
 // update the birth to 02/12/1998 where name set to "Koren"
-const updateKorenBD = () => {
-  Student.updateMany({ name: "Koren" }, { birth: new Date("1998/12/02Z") })
-    .then(() => {
-      return "updated";
-    })
-    .catch(() => {
-      return "failed";
-    });
+const updateKorenBD = async () => {
+  try {
+    await Student.updateMany(
+      { name: "Koren" },
+      { birth: new Date("1998/12/02Z") }
+    );
+    return "updated";
+  } catch (error) {
+    return false;
+  }
 };
